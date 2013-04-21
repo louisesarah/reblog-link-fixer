1ac24b2b72d4d0687632f6dbebe4c0afb47d344f
ehudc <s.ehud.cohen@gmail.com>
2013-04-21 19:46:22 -0400

    manifest and background.js added, README updated

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 // background.js:
 // React when browser action's icon is clicked (turn on/off)
 
-var status = false;
+var toggle = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
-  status = !status;
-  if(status){
+  toggle = !toggle;
+  if(toggle){
     chrome.browserAction.setIcon({path: "link_enabled.png", tabId:tab.id});
-    chrome.tabs.executeScript(tab.id, file:"reblogLinkScript.user.js");
+    chrome.tabs.executeScript(tab.id, {file:"reblogLinkScript.user.js"});
   }
   else{
     chrome.browserAction.setIcon({path: "link_disabled.png", tabId:tab.id});
-    chrome.tabs.executeScript(tab.id, code:"alert()");
+    chrome.tabs.executeScript(tab.id, {code:""});
   }
 });
